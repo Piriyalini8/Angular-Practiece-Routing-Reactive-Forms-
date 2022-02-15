f68e5e0ba67f0a78cc30ac00a8a04e2be107b289
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2022-02-15 10:19:09 +0530

    Form Control only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { PartTimeJobComponent } from './part-time-job/part-time-job.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserAuthModule
+    UserAuthModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
