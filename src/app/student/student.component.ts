c3d9c235be9c3cf5f07c58f6b7c292a3f4505ffb
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2022-02-16 23:00:40 +0530

    Form Builder, setData, patchData

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student',
@@ -8,29 +8,64 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class StudentComponent implements OnInit {
 
-  profileName =new FormGroup({
-    firstName : new FormControl('',[Validators.required,Validators.minLength(5)]),
-    lastName : new FormControl('',[Validators.required,Validators.minLength(3)]),
-    emailPassword: new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl('')
-    }),
-    address: new FormGroup({
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
-  })
-  
-  gender= new FormControl('')
-  checkBox=new FormControl([])
+  // profileName =new FormGroup({
+  //   firstName : new FormControl('',[Validators.required,Validators.minLength(5)]),
+  //   lastName : new FormControl('',[Validators.required,Validators.minLength(3)]),
+  //   emailPassword: new FormGroup({
+  //     email: new FormControl(''),
+  //     password: new FormControl('')
+  //   }),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     zip: new FormControl('')
+  //   }),
+  //   gender: new FormControl('')
+  // })
 
-  constructor() { }
+  profileName!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.profileName=this.fb.group({
+      firstName:['Hello',[Validators.required,Validators.minLength(5)]],
+      lastName:['',[Validators.required,Validators.minLength(3)]],
+      emailPassword:this.fb.group({
+        email:[''],
+        password:['']
+      }),
+      address:this.fb.group({
+        city:[''],
+        state:[''],
+        zip:['']
+      }),
+      gender:['']
+    })
   }
   onSubmit(){
-    console.log(this.profileName.value,this.gender.value)
+    console.log(this.profileName.value)
+  }
+  setData(){
+    // this.profileName.setValue({
+    //   firstName:'Priyalini',
+    //   lastName:'Niththiyanantham',
+    //   emailPassword:{
+    //     email:'priya@gmail.com',
+    //     password:'password'
+    //   },
+    //   address:{
+    //     city:'Batticaloa',
+    //     state:'opt1',
+    //     zip:'30200'
+    //   },
+    //   gender:'female'
+    // })
+
+    this.profileName.patchValue({
+      firstName:'Priyalini',
+      lastName:'Niththiyanantham'
+    })
   }
 
 }
